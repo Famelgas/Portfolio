43601e7fce247d91ed1e6b12dff3fd58c1d51f66
Famelgas <filipe.damendes@gmail.com>
2023-06-29 16:13:33 +0100

    buttons criados

---
+++ portfolio/src/components/Header.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+  
+export default function Header() {
+  return (
+    <header className="App-header">
+      <h1>Portfolio</h1>
+      <div className="header_buttons">
+        <button>Button 1</button>
+        <button>Button 2</button>
+        <button>Button 3</button>
+      </div>
+    </header>
+
+  );
+}
