b6098ff9d10534008a9f9a836127fb7b649cc01a
Famelgas <filipe.damendes@gmail.com>
2023-06-30 17:41:59 +0100

    buttons criados

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -1,15 +1,59 @@
 import * as React from "react";
   
 export default function Header() {
-  return (
-    <header className="App-header">
-      <h1>Portfolio</h1>
-      <div className="header_buttons">
-        <button>Button 1</button>
-        <button>Button 2</button>
-        <button>Button 3</button>
-      </div>
-    </header>
+    return (
+        <header className="App-header">
+            <h1>Portfolio</h1>
+            <div className="header_buttons">
+                <ButtonHome/>
+                <ButtonAbout/>
+                <ButtonPhotos/>
+            </div>
+        </header>
 
-  );
+    );
 }
+
+export function ButtonHome() {
+    const [isActive, setIsActive] = React.useState(false);
+
+    const handleButtonClick = () => {
+        setIsActive(!isActive);
+    };
+    
+    return (
+        <button onClick={handleButtonClick}>
+            Home
+        </button>
+    );
+}
+
+
+export function ButtonAbout() {
+    const [isActive, setIsActive] = React.useState(false);
+
+    const handleButtonClick = () => {
+        setIsActive(!isActive);
+    };
+
+    return (
+        <button onClick={handleButtonClick}>
+            About
+        </button>
+    );
+}
+
+export function ButtonPhotos() {
+    const [isActive, setIsActive] = React.useState(false);
+
+    const handleButtonClick = () => {
+        setIsActive(!isActive);
+    };
+
+    return (
+        <button onClick={handleButtonClick}>
+            Photos
+        </button>
+    );
+}
+
