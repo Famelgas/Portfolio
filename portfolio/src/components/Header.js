e0432bdcab1845af878acb1e8775bbb9694cddc6
JonnyMoura <jmfmoura.123@gmail.com>
2023-06-30 21:58:32 +0100

    estrutura mais ou menos completa

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -1,70 +1,31 @@
-import * as React from "react";
-  
-export default function Header() {
-    const [home_button, setHomeButton] = React.useState(false);
-    const [about_button, setAboutButton] = React.useState(false);
-    const [photos_button, setPhotosButton] = React.useState(false);
+import React from "react";
 
-    const updateHomeButton = (updated_home_button) => {
-        setHomeButton(updated_home_button);
-      };
 
-    return (
-        <header className="App-header">
-            <h1>Portfolio</h1>
-            <div className="header_buttons">
-                <ButtonHome home_button={home_button} updated_home_button={updateHomeButton} />
-                <ButtonAbout/>
-                <ButtonPhotos/>
-            </div>
-        </header>
-
-    );
+export  function ButtonHome({ isActive, onClick }) {
+  return (
+    <button onClick={onClick}>
+      {isActive ? "Active" : "Inactive"} Home
+    </button>
+  );
 }
 
-export function ButtonHome(props) {
-    const { home_button, updateHomeButton } = props;
-    const [isActive, setIsActive] = React.useState(false);
+export   function ButtonAbout({ isActive, onClick }) {
+  return (
+    <button onClick={onClick}>
+      {isActive ? "Active" : "Inactive"} About
+    </button>
+  );
+}
 
-    const handleButtonClick = () => {
-        const updated_home_button = !home_button;
-        updateHomeButton(updated_home_button)
-        setIsActive(!isActive);
-    };
-    
-    return (
-        <button onClick={handleButtonClick}>
-            {home_button ? "Home" : "not Home"}
-        </button>
-    );
+export   function ButtonPhotos({ isActive, onClick }) {
+  return (
+    <button onClick={onClick}>
+      {isActive ? "Active" : "Inactive"} Photos
+    </button>
+  );
 }
 
 
-export function ButtonAbout() {
-    const [isActive, setIsActive] = React.useState(false);
 
-    const handleButtonClick = () => {
-        setIsActive(!isActive);
-    };
 
-    return (
-        <button onClick={handleButtonClick}>
-            About
-        </button>
-    );
-}
-
-export function ButtonPhotos() {
-    const [isActive, setIsActive] = React.useState(false);
-
-    const handleButtonClick = () => {
-        setIsActive(!isActive);
-    };
-
-    return (
-        <button onClick={handleButtonClick}>
-            Photos
-        </button>
-    );
-}
-
+/* removi a funçao header porque dado que o html ja tem a tag header nao faz sentido estar a complicar dado que necessitamos de guardar o estado dos botoes. Optei por fazer a relação de herança do componente 'App' diretamente com os botoes*/
